b6e173e3a35b637f13c09250f9e236f13a090f1d
maxmasterit <li3685@gmail.com>
2017-06-18 19:47:25 -0400

    added main method and more print statements

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
-a = 1
-b = 2
-c = 3
-print('Hello World', end = ' ')
-print('%d' % (c))
+def main():
+    print("main method executed")
+    a = 1
+    b = 2
+    c = 3
+    print('Hello World', end = ' ')
+    print('%d' % (a))
+    print('%d' % (b))
+    print('%d' % (c))
+if __name__ == '__main__':
+    main()
 
