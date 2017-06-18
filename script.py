160b4e789e763bf7e3fa97aac377b6c683305aa2
maxmasterit <li3685@gmail.com>
2017-06-18 19:57:29 -0400

    multiplied variables by 10

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 def main():
     print("main method executed!!!")
-    a = 1
-    b = 2
-    c = 3
+    a = 10
+    b = 20
+    c = 30
     print('Hello World', end = ' ')
     print('%d' % (a))
     print('%d' % (b))
